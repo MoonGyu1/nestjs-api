642f679428d29661ac50c108ccbc9480c533f2b3
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-01-05 17:02:19 +0900

    DTO and Validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // 프로퍼티를 원하는 타입으로 변경
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
