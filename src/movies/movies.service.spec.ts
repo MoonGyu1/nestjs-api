5a523a9d93b3a51873a5f6c18f25d054c43da902
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-01-05 17:50:18 +0900

    basic unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -4,6 +4,7 @@ import { MoviesService } from './movies.service';
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 각 테스트 전에 실행되는 부분
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -14,5 +15,9 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Pass', () => {
+    expect(2 + 3).toEqual(5);
   });
 });
